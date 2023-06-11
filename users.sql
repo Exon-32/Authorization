4f19ba857d0ec3a5e3193a120404e78947ae9af5
Sujal <111888102+Exon-32@users.noreply.github.com>
2023-06-11 07:28:17 +0545

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,64 +1,23 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 10, 2023 at 05:52 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `db_authorize`
 --
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `user_id` int(10) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `password`) VALUES
-(0, 'kart', 'p@ssword'),
-(1, 'manish', 'p@ssword'),
-(2, 'Ramesh', 'p@ssword'),
-(3, 'ram', 'p@ssword');
+INSERT INTO `users` (`username`, `password`) VALUES
+('kart', 'p@ssword'),
+('manish', 'p@ssword'),
+('Ramesh', 'p@ssword'),
+('ram', 'p@ssword');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `username_3` (`username`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `user_id_2` (`user_id`),
-  ADD KEY `username` (`username`),
-  ADD KEY `username_2` (`username`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE users ADD user_id INT PRIMARY KEY AUTO_INCREMENT;
